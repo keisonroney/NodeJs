649f1c99b9e7933279f472f43bcb298d4f6fed42
Keison Roney <keisonroney@gmail.com>
2024-01-19 18:37:22 -0300

    novos arquivos

---
+++ 02modulos/app.js
@@ -0,0 +1,10 @@
+// importar arquivo de cálculos
+var calculos = require ('./calculos');
+
+
+//obter a função para multiplicar
+var multiplicar = calculos.multiplicar;
+
+
+//Realizar um cálculo de multiplicação
+console.log(multiplicar(5, 6));
