b8cf3146d2302c16f7ba8a8ec5dfffe5dd1bdbef
Keison Roney <keisonroney@gmail.com>
2024-01-22 17:01:50 -0300

    novos arquivos

---
+++ 05html/app.js
@@ -0,0 +1,12 @@
+// Importar o Express
+var express = require('express');
+
+//App
+var app = express();
+
+app.get('/', function(req,res){
+    res.sendFile(__dirname + '/pagina.html');
+});
+
+// Servidor 
+app.listen(8080);
