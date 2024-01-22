71c3cd1390c8e14d3e1bcc8641c29f18df84162d
Keison Roney <keisonroney@gmail.com>
2024-01-22 18:36:59 -0300

    novos arquivos

--- 05html/app.js
+++ 05html/app.js
@@ -4,6 +4,9 @@ var express = require('express');
 //App
 var app = express();
 
+// Especificar local do CSS e da imagem
+app.use(express.static(__dirname + '/publico'));
+
 app.get('/', function(req,res){
     res.sendFile(__dirname + '/pagina.html');
 });
