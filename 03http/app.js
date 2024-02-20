460ce04360f7af878a748edb8829109a9f202153
Keison Roney <110565730+keisonroney@users.noreply.github.com>
2024-02-20 17:50:27 -0300

    Delete 03http directory

--- 03http/app.js
+++
@@ -1,8 +0,0 @@
-//Importar módulo Http
-var http = require('http')
-
-// Criar o servidor
-http.createServer(function(req,res){
-    res.write('Utilizando o Nodemon');
-    res.end();
-}).listen(8080);
